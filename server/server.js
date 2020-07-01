994ae3613fc7cafff3463d1cf08167311f6b6856
unknown <msaric1995@gmail.com>
2020-07-01 22:01:21 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser")
 const app = express();
 
 var port = process.env.PORT || 5000;
+const publicPath = path.join(__dirname, '..', 'public');
 app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
@@ -19,6 +20,7 @@ app.use('/kupnja', Kupnja)
 
 var Vina = require('./route/VinaRoute')
 app.use('/vina', Vina)
+app.use(express.static(publicPath));
 
 app.listen(port, ()=> {
  console.log("Server listening on port: " + port)
